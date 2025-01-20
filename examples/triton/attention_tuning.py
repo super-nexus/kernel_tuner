9cbdc7f0ff9ddb423528d96486f694a0ee9fe1fd
super-nexus <andrija.kuzmanov@gmail.com>
2025-01-20 09:20:22 +0000

    fix: syntax error

--- examples/triton/attention_tuning.py
+++ examples/triton/attention_tuning.py
@@ -254,10 +254,10 @@ def tune_attention(batch_size=2, seq_len=128, head_dim=64, num_heads=4):
         grid_div_y=grid_div_y,
         grid_div_z=grid_div_z,
         block_size_names=['BLOCK_M', 'BLOCK_N'],
-        strategy='genetic_algorithm'
+        strategy='genetic_algorithm',
         strategy_options={
-            'maxiter=10000'
-        }
+            'maxiter': 10000
+        },
     )
 
     return results
