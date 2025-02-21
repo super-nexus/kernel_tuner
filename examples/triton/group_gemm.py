e24aaf61fd9b59fa04174b1614d3351e07d5f677
akuzmanov <andrija.kuzmanov@gmail.com>
2025-02-21 17:03:45 +0100

    chore: change group_gemm strategy to GA

--- examples/triton/group_gemm.py
+++ examples/triton/group_gemm.py
@@ -177,6 +177,10 @@ def tune_group_gemm(N):
         lang="TRITON",
         block_size_names=["BLOCK_SIZE_X", "BLOCK_SIZE_Y", "BLOCK_SIZE_Z"],
         cache=cache_file_name,
+        strategy="genetic_algorithm",
+        strategy_options={
+            'maxiter': 10000
+        },
     )
     
     # Filter out failed configurations and format results
