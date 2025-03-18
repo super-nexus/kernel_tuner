664fbedf5ad5c4451b691383e7f4bd2a4e21a751
super-nexus <andrija.kuzmanov@gmail.com>
2025-03-18 13:06:35 +0000

    fix: block names

--- examples/triton/parallel_triton_compilation.py
+++ examples/triton/parallel_triton_compilation.py
@@ -110,9 +110,9 @@ def main(m):
     start_time = time.time()
 
     tune_params = dict()
-    tune_params['BLOCK_SIZE_X'] = [16 * 2 ** i for i in range(6)]
-    tune_params['BLOCK_SIZE_Y'] = [16 * 2 ** i for i in range(6)]
-    tune_params['BLOCK_SIZE_Z'] = [16 * 2 ** i for i in range(6)]
+    tune_params['BLOCK_SIZE_M'] = [16 * 2 ** i for i in range(6)]
+    tune_params['BLOCK_SIZE_N'] = [16 * 2 ** i for i in range(6)]
+    tune_params['BLOCK_SIZE_K'] = [16 * 2 ** i for i in range(6)]
     tune_params['num_stages'] = [1, 2, 3, 4, 5]
     tune_params['num_warps'] = [2, 4, 8]
     tune_params['GROUP_SIZE_M'] = [i for i in range(4, 10)]
