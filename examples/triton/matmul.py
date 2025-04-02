799e4de695841173414e52e47ddf0e3347daacf1
super-nexus <andrija.kuzmanov@gmail.com>
2025-04-02 10:17:59 +0000

    chore: try brute force

--- examples/triton/matmul.py
+++ examples/triton/matmul.py
@@ -160,8 +160,6 @@ def tune_matmul(m):
         lang='TRITON',
         block_size_names=["BLOCK_SIZE_X", "BLOCK_SIZE_Y", "BLOCK_SIZE_Z"],
         cache=cache_file_name,
-        strategy='genetic_algorithm',
-        strategy_options={'maxiter': 1000},
         restrictions=restrictions,
    )
 
